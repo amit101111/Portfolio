1904009e248b944ecb252bdbf276ea80012951fc
Amit Sarker <amit1999cse47@gmail.com>
2024-01-10 22:55:35 +0600

    10-01-24 work

--- ui_form.py
+++ ui_form.py
@@ -15,9 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QMainWindow,
-    QMenuBar, QSizePolicy, QStatusBar, QVBoxLayout,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QLabel,
+    QMainWindow, QMenuBar, QSizePolicy, QStatusBar,
+    QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -26,27 +26,92 @@ class Ui_MainWindow(object):
         MainWindow.resize(800, 600)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.verticalLayout = QVBoxLayout(self.centralwidget)
-        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout_2 = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.frame = QFrame(self.centralwidget)
         self.frame.setObjectName(u"frame")
-        self.frame.setStyleSheet(u"QFrame{	\n"
-"	background-color: rgb(107, 98, 200);\n"
-"	color: rgb(15, 15, 61);\n"
-"	font: 700 italic 12pt \"Segoe UI\";\n"
-"	text-decoration: underline;\n"
-"}")
         self.frame.setFrameShape(QFrame.StyledPanel)
         self.frame.setFrameShadow(QFrame.Raised)
-        self.verticalLayout_2 = QVBoxLayout(self.frame)
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.label = QLabel(self.frame)
+        self.verticalLayout = QVBoxLayout(self.frame)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.frame_2 = QFrame(self.frame)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setFrameShape(QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.gridLayout_2 = QGridLayout(self.frame_2)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.label = QLabel(self.frame_2)
         self.label.setObjectName(u"label")
+        font = QFont()
+        font.setPointSize(18)
+        font.setBold(True)
+        self.label.setFont(font)
+        self.label.setLayoutDirection(Qt.LeftToRight)
+        self.label.setFrameShape(QFrame.Panel)
+        self.label.setFrameShadow(QFrame.Raised)
+        self.label.setLineWidth(4)
 
-        self.verticalLayout_2.addWidget(self.label, 0, Qt.AlignHCenter)
+        self.gridLayout_2.addWidget(self.label, 0, 0, 1, 1, Qt.AlignHCenter|Qt.AlignVCenter)
 
 
-        self.verticalLayout.addWidget(self.frame, 0, Qt.AlignTop)
+        self.verticalLayout.addWidget(self.frame_2, 0, Qt.AlignHCenter|Qt.AlignTop)
+
+        self.gridLayout = QGridLayout()
+        self.gridLayout.setSpacing(3)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.label_3 = QLabel(self.frame)
+        self.label_3.setObjectName(u"label_3")
+
+        self.gridLayout.addWidget(self.label_3, 0, 0, 1, 1)
+
+        self.label_4 = QLabel(self.frame)
+        self.label_4.setObjectName(u"label_4")
+
+        self.gridLayout.addWidget(self.label_4, 0, 1, 1, 1)
+
+        self.label_5 = QLabel(self.frame)
+        self.label_5.setObjectName(u"label_5")
+
+        self.gridLayout.addWidget(self.label_5, 1, 0, 1, 1)
+
+        self.label_6 = QLabel(self.frame)
+        self.label_6.setObjectName(u"label_6")
+
+        self.gridLayout.addWidget(self.label_6, 1, 1, 1, 1)
+
+
+        self.verticalLayout.addLayout(self.gridLayout)
+
+        self.frame_3 = QFrame(self.frame)
+        self.frame_3.setObjectName(u"frame_3")
+        self.frame_3.setAutoFillBackground(False)
+        self.frame_3.setFrameShape(QFrame.NoFrame)
+        self.frame_3.setFrameShadow(QFrame.Raised)
+        self.frame_3.setLineWidth(2)
+        self.gridLayout_3 = QGridLayout(self.frame_3)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.label_2 = QLabel(self.frame_3)
+        self.label_2.setObjectName(u"label_2")
+        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy)
+        font1 = QFont()
+        font1.setPointSize(6)
+        self.label_2.setFont(font1)
+
+        self.gridLayout_3.addWidget(self.label_2, 0, 0, 1, 1, Qt.AlignRight|Qt.AlignBottom)
+
+
+        self.verticalLayout.addWidget(self.frame_3, 0, Qt.AlignRight|Qt.AlignBottom)
+
+        self.verticalLayout.setStretch(0, 2)
+        self.verticalLayout.setStretch(1, 20)
+        self.verticalLayout.setStretch(2, 1)
+
+        self.verticalLayout_2.addWidget(self.frame)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
@@ -64,6 +129,11 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"AMIT SARKER KISHOR", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Welcome to my Portfolio Website", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"@Copyright 2024 by Amit Sarker Kishor", None))
     # retranslateUi
 
